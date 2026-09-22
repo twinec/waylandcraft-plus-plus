78f5d938c8f745029d2a77e747f9b333dca8ad84
Evelyn <73487868+EVV1E@users.noreply.github.com>
2026-09-22 22:48:07 +0200

    Implement detached cursor option (#217)

--- src/main/java/dev/evvie/waylandcraft/gui/WaylandCraftSettingsScreen.java
+++ src/main/java/dev/evvie/waylandcraft/gui/WaylandCraftSettingsScreen.java
@@ -85,6 +85,7 @@ public class WaylandCraftSettingsScreen extends Screen {
 		createIntSettingsWidget(WaylandCraftSettings.PIXELS_PER_BLOCK, Component.literal("Window display pixels per block"));
 		createBooleanSettingsWidget(WaylandCraftSettings.FOCUS_ON_HOVER, Component.literal("Focus windows when hovered"));
 		createTextSettingsWidget(WaylandCraftSettings.TERMINAL_CHOICE, Component.literal("Default terminal"));
+		createBooleanSettingsWidget(WaylandCraftSettings.DETACH_CURSOR, Component.literal("Detach cursor in capture mode"));
 	}
 	
 }
--- src/main/java/dev/evvie/waylandcraft/settings/WaylandCraftSettings.java
+++ src/main/java/dev/evvie/waylandcraft/settings/WaylandCraftSettings.java
@@ -21,16 +21,19 @@ public class WaylandCraftSettings {
 	int pixelsPerBlock = 500;
 	boolean focusOnHover = false;
 	String terminalChoice = "";
+	boolean detachCursor = false;
 	
 	/* This is where the field names go to avoid typos */
 	public static final String PIXELS_PER_BLOCK = "pixelsPerBlock";
 	public static final String FOCUS_ON_HOVER = "focusOnHover";
 	public static final String TERMINAL_CHOICE = "terminalChoice";
+	public static final String DETACH_CURSOR = "detachCursor";
 	
 	public static final String[] SETTINGS = new String[] {
 			PIXELS_PER_BLOCK,
 			FOCUS_ON_HOVER,
-			TERMINAL_CHOICE
+			TERMINAL_CHOICE,
+			DETACH_CURSOR
 	};
 	
 	/* This is where the getters go */
@@ -45,6 +48,10 @@ public class WaylandCraftSettings {
 	
 	public String getTerminalChoice() {
 		return terminalChoice;
+	}
+	
+	public boolean getDetachCursor() {
+		return detachCursor;
 	}
 	
 	/* Methods to modifiy settings by name */
